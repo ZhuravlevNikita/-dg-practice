67ceb4cd6c3d4e78169588078fcfa90ae59503bc
Nikita Zhuravlev <mckedonsky@gmail.com>
2022-08-03 11:58:50 +0300

    student work

--- student.rb
+++ student.rb
@@ -1,10 +1,14 @@
 class Where
   def self.run
-    puts "Enter age"
-    age = gets.chomp
-    file = File.open('results.txt', 'a')
-    File.foreach('students.txt') {|student| file.puts (student if student.include? (age))}
-    file.close
+    loop do
+      puts "Enter age"
+      age = gets.to_i
+      break if age == -1
+      file = File.open('results.txt', 'a')
+      text = File.readlines('students.txt').map(&:chomp)
+      text.each {|student| file.puts student if student[-2..-1].to_i == age}
+      file.close
+    end
   end
 end
 
